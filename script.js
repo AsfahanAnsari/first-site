18bdf4fab19235eb24d0646195424826a811424e
Asfahan Ansari <asfahanansari279@gmail.com>
2026-09-22 23:18:20 +0530

    compete
    complete java

--- script.js
+++ script.js
@@ -1,46 +1,39 @@
 // ========================================
 // NEXORA - JAVASCRIPT
 // ========================================
- 
-
 
 
 // ---------- CART ----------
 
-
-
 let cart = [];
 
 
-// Find all "Addto cart" buttons 
-const addtocartbutton =
-document.querySelectorAll(".product-card button");
+// Find all "Add to Cart" buttons
+const addToCartButtons =
+    document.querySelectorAll(".product-card button");
 
 
-//Find the cart button 
-const cartbutton =
-document.getElementById("cart-button")
+// Find the Cart button
+const cartButton =
+    document.getElementById("cartButton");
 
 
-//Add click event to every product button
-addtocartbutton.forEach(function(button) {
+// Add click event to every product button
+addToCartButtons.forEach(function(button) {
 
-    button.addEventListener("click",function() { 
+    button.addEventListener("click", function() {
 
-
-        //Find the product card
+        // Find the product card
         const productCard =
-        button.closest(".product-card");
+            button.closest(".product-card");
 
-        //Get product name
+        // Get product name
         const productName =
-        productCard.querySelector("h3").textContent;
-        
-        
+            productCard.querySelector("h3").textContent;
+
         // Get product price
         const productPrice =
-        productCard.querySelector("strong").textContent; 
-
+            productCard.querySelector("strong").textContent;
 
 
         // Add product to cart
@@ -50,35 +43,62 @@ addtocartbutton.forEach(function(button) {
         });
 
 
-        //show notification
-        alert(productName + " Added to cart!")
+        // Show notification
+        alert(productName + " added to cart!");
 
 
-
-        //update cart button
-        updatecartButton();
-
+        // Update cart button
+        updateCartButton();
 
     });
+
 });
-
-
 
 
 // ---------- UPDATE CART BUTTON ----------
 
-function updateCart Button() {
+function updateCartButton() {
 
     cartButton.textContent =
-     "🛒 Cart (" + cart.length + ")";
+        "🛒 Cart (" + cart.length + ")";
 
 }
-
 
 
 // ---------- OPEN CART ----------
 
 cartButton.addEventListener("click", function() {
-    
-    if (cart.length === 0)
+
+    if (cart.length === 0) {
+
+        alert("Your cart is empty!");
+
+        return;
+    }
+
+
+    let cartMessage =
+        "🛒 YOUR NEXORA CART\n\n";
+
+
+    cart.forEach(function(product, index) {
+
+        cartMessage +=
+            (index + 1) +
+            ". " +
+            product.name +
+            " - " +
+            product.price +
+            "\n";
+
+    });
+
+
+    cartMessage +=
+        "\nTotal items: " +
+        cart.length;
+
+
+    alert(cartMessage);
+
 });
